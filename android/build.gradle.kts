a568a70bee9a5e4033410fc07d79fc0c024002c2
assadullah381 <BSCS23213@itu.edu.pk>
2025-03-27 15:19:48 +0500

    database connected

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,24 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.1")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
+
 rootProject.buildDir = file("../build")
 
 subprojects {
-    buildDir = file("${rootProject.buildDir}/${name}")
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
 }
 
 tasks.register<Delete>("clean") {
